17ebd2f7e2ca3f31f889dc05fcc0e88cf7ac7e37
Raman Singh <ramansinghchauhan787@gmail.com>
2026-07-30 13:24:58 +0545

    done

--- weather_api_v2/main.py
+++ weather_api_v2/main.py
@@ -8,3 +8,22 @@ print("Status Code:", res.status_code)
 
 data = res.json()
 print(data[0]["name"])
+
+
+def division():
+    try:
+        print("Opening Application")
+        number = 10 / 0
+        print(f"the number is {number}")
+    except ZeroDivisionError:
+        print("cannot divide by 0")
+    else:
+        print("Everything executed successfully")
+
+    finally:
+        print("Program run successfully")
+
+
+
+
+division()
